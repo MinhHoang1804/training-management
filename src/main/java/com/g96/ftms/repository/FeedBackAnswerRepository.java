23671b6be8d4563b956d7aa5b430e68d0df557c4
DungBA <lethily270105@gmail.com>
2024-11-17 19:51:30 +0700

    fix(feedback):search feedback

--- src/main/java/com/g96/ftms/repository/FeedBackAnswerRepository.java
+++ src/main/java/com/g96/ftms/repository/FeedBackAnswerRepository.java
@@ -3,9 +3,14 @@ package com.g96.ftms.repository;
 import com.g96.ftms.entity.FeedbackAnswer;
 import com.g96.ftms.entity.FeedbackAnswerRelationId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FeedBackAnswerRepository extends JpaRepository<FeedbackAnswer, FeedbackAnswerRelationId> {
     long deleteByFeedback_FeedbackId(Long feedbackId);
+
+    @Query("SELECT AVG(fa.rating) FROM FeedbackAnswer fa WHERE fa.id.feedbackId = :feedbackId")
+    Double findAverageRatingByFeedbackId(@Param("feedbackId") Long feedbackId);
 }
