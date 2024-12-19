6ea1357ba7a3395173f9afc6f83f902bcd06feda
DungBA <lethily270105@gmail.com>
2024-12-19 15:49:35 +0700

    feat(subject):update subject

--- src/main/java/com/g96/ftms/repository/ScheduleRepository.java
+++ src/main/java/com/g96/ftms/repository/ScheduleRepository.java
@@ -13,7 +13,8 @@ import java.util.List;
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     long deleteBySubject_SubjectIdAndClasss_ClassId(Long subjectId, Long classId);
     List<Schedule> findByClasss_ClassIdAndSubject_SubjectId(Long classId, Long subjectId);
-    @Query("SELECT distinct s.trainer FROM Schedule s WHERE s.startDate <= :startDate AND s.endDate >= :endDate")
+    @Query("SELECT DISTINCT s.trainer FROM Schedule s WHERE " +
+            "(s.startDate <= :endDate AND s.endDate >= :startDate)")
     List<String> findTraineesInRangeAndSlot(
                                             @Param("startDate") LocalDateTime startDate,
                                             @Param("endDate") LocalDateTime endDate);
